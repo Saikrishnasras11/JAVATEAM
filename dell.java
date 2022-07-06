e8127d8158d34e954f8910d1619d69dd005a82c4
OGGARINIRESH <108750463+OGGARINIRESH@users.noreply.github.com>
2022-07-06 21:28:45 +0530

    changed it
    thank you

--- dell.java
+++ dell.java
@@ -1,1 +1,3 @@
 hi iam here to say the we are going to adjust the cover of project code in the front view need to know that we are great to announce that we are not able to conduct the exam in this month so we will  sheduled as we requred need in it 
+hi NIRESH this message is for you to complete the project with your team member as soon as
+Thank you for taking responsbility 

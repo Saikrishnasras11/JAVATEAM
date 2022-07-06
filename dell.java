f6d2ff75552ffbd47c00aad5fbe22567377b5b91
niresh <oggariniresh9@gmail.com>
2022-07-06 21:15:27 +0530

    java team members only

---
+++ dell.java
@@ -0,0 +1,1 @@
+hi iam here to say the we are going to adjust the cover of project code in the front view need to know that we are great to announce that we are not able to conduct the exam in this month so we will  sheduled as we requred need in it 
